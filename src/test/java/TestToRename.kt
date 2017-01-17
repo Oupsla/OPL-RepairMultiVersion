6cc1caa68fee102c2e10c5b2fbd2136783aac119
Benjamin Coenen <benjamin.coenen@hotmail.com>
2017-01-17 17:07:19 +0100

    test(test): delete example tests

--- src/test/java/TestToRename.kt
+++ src/test/java/TestToRename.kt
@@ -6,34 +6,9 @@ import util.GitUtils
  */
 class TestToRename {
 
-    @Before
-    fun beforeTesting() {
-        println("------------------")
-        println("avant test")
-    }
-
-    @After
-    fun afterTesting(){
-        println("apres test")
-        println("------------------")
-    }
-
-    @Test
-    fun testT1() {
-        println("exec Test1")
-        Assert.assertEquals(1, 1 * 1)
-    }
-
     @Test
     fun testGitUtils() {
         Assert.assertEquals(GitUtils.cloneRepo("https://github.com/Oupsla/OPL-TestRepo", "test-repo"), "C:\\Users\\Apolloch\\IdeaProjects\\OPL-RepairMultiVersion\\tmp\\opltest-repo/master/.git")
-    }
-
-    @Ignore
-    @Test
-    fun testT2() {
-        println("exec Test2")
-        Assert.assertEquals(1,2)
     }
 
 }
