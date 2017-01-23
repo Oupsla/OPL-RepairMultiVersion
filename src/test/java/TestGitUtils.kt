bee790397d1deb545d4cc1c69e1997dcc2bc4ef6
bacquet <nathan.bacquet@etudiant.univ-lille1.fr>
2017-01-23 13:18:17 +0100

    rename tests

--- src/test/java/TestGitUtils.kt
+++ src/test/java/TestGitUtils.kt
@@ -4,10 +4,10 @@ import util.GitUtils
  * Created by Apolloch on 29/12/2016.
  *to launch tests : mvn test
  */
-class TestToRename {
+class TestGitUtils {
 
     @Test
-    fun testGitUtils() {
+    fun testCloneRepository() {
         Assert.assertEquals(GitUtils.cloneRepo("https://github.com/Oupsla/OPL-TestRepo", "test-repo"), "C:\\Users\\Apolloch\\IdeaProjects\\OPL-RepairMultiVersion\\tmp\\opltest-repo/master/.git")
     }
 
